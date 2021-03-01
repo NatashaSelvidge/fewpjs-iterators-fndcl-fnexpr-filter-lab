9d750d4f78a82bff883a97c456883771ecc6b983
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-01 12:04:30 -0800

    commit tree 8ff1a2fdc088f95cff1f16966aa927f0ddff9190

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, string){
+  return array.filter( possibleMatch => 
+    possibleMatch.toLowerCase() === string.toLowerCase()
+    )
+}
+
+
+function fuzzyMatch(array, string){
+  return array.filter( possibleMatch => 
+    possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0 
+    )
+}
+
+function matchName(array, string){
+  return array.filter(match => match.name === string);
+}
